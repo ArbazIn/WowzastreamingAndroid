ad8b4ab8b14e86df0b9e62014ae1de5fb980da3a
bozdoz <arbaz.thetatehnolabs@gmail.com>
2016-09-29 17:24:14 +0530

    GoLive

---
+++ app/src/main/java/com/ttls/golive/AppConfig.java
@@ -0,0 +1,11 @@
+package com.ttls.golive;
+
+/**
+ * Created by arbaz on 29/9/16.
+ */
+
+public class AppConfig {
+    public static final String STREAM_URL = "rtsp://192.168.1.5:1935/live/myStream";//PC URL(BUT must use public ip ie. WOWZA ip(192.168.64.1:1935/live/myStream))
+    public static final String PUBLISHER_USERNAME = "arbaz_theta";
+    public static final String PUBLISHER_PASSWORD = "theta1234";
+}
